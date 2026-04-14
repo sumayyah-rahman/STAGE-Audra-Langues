f7cbcda7dfcb609387477175eefe31cae6b15a51
sumayyah-rahman <sumayyah.rahman05@gmail.com>
2026-04-14 10:56:40 +0200

    Update and rename login_page.html to portail_eleve.php

--- PORTAIL ELEVE/portail_eleve.php
+++ PORTAIL ELEVE/portail_eleve.php
@@ -1,3 +1,22 @@
+<?php
+declare(strict_types=1);
+
+require_once __DIR__ . '/../../app/security/firewall.php';
+$config = audra_bootstrap_prof_page(); // ou autre bootstrap commun si vous en avez un
+
+if (($config['env'] ?? 'DEV') === 'DEV') {
+    ini_set('display_errors', '1');
+    error_reporting(E_ALL);
+} else {
+    ini_set('display_errors', '0');
+    ini_set('log_errors', '1');
+    error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING & ~E_DEPRECATED);
+}
+
+// Ici : logique élève
+// ex. vérifier si l’élève est connecté
+// sinon redirection vers login élève
+?>
 <!DOCTYPE html>
 <html>
     <head>
